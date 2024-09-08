a7442e1f93d781519c692089bb7d5f55b037ad66
Victor Hugo <vhlima1008@gmail.com>
2024-09-08 00:40:12 -0300

    Remove clicks in non null squares. Added simple score and round count. New AI implement will come.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
-import TicTacToe from './Components/TicTacToe';
+import TicTacToe from './Tic-Tac-Toe/TicTacToe';
+import AITicTacToe from './AI-Tic-Tac-Toe/TicTacToe-AI';
 
 export default function App(){
   return (
     <>
+      <AITicTacToe/>
       <TicTacToe/>
     </>
   );
--- src/Tic-Tac-Toe/TicTacToe.tsx
+++ src/Tic-Tac-Toe/TicTacToe.tsx
@@ -13,9 +13,14 @@ let o_css = "background-color:#dff8ff; padding:10px; border-radius:8px;";
 
 export default function TicTacToe(){
 
+    let [move, setMove] = useState(0);
     let [count, setCount] = useState(0);
+    let [xWins, setXWins] = useState(0);
+    let [oWins, setOWins] = useState(0);
+    let [draw, setDraws] = useState(0);
     let [lock, setLock] = useState(false);
     let titleRef = useRef(null);
+    let roundRef = useRef(null);
     let box1 = useRef(null);
     let box2 = useRef(null);
     let box3 = useRef(null);
@@ -27,24 +32,32 @@ export default function TicTacToe(){
     let box9 = useRef(null);
     let box_array = [box1,box2,box3,box4,box5,box6,box7,box8,box9];
 
-    const toggle = (e,num) => {
-        if(lock){
-            return 0;
-        }
-        if(count%2===0){
-            titleRef.current.innerHTML = `Round: <img style='${o_css}' src='${O_icon}'>`;
-            e.target.innerHTML = `<img style='${X_css}' src='${X_icon}'>`;
-            data[num]="x";
-            setCount(++count);
-        }else{
-            titleRef.current.innerHTML = `Round: <img style='${x_css}' src='${X_icon}'>`;
-            e.target.innerHTML = `<img style='${O_css}' src='${O_icon}'>`;
-            data[num]="o";
-            setCount(++count);
-        }
-        checkWin()
+    const score = () =>{
+        roundRef.current.innerHTML = `X: ${xWins} | O: ${oWins} | Draws: ${draw}`;
     }
-    
+
+    const toggle = (e,num) => {
+        if(data[num]=="x"||data[num]=="o"){
+            data[num]=data[num];
+        }else{
+            if(lock){
+                return 0;
+            }else if(count%2===0){
+                titleRef.current.innerHTML = `Round ${count}: <img style='${o_css}' src='${O_icon}'>`;
+                e.target.innerHTML = `<img style='${X_css}' src='${X_icon}'>`;
+                data[num]="x";
+                setCount(++count);
+                setMove(move=num);
+            }else{
+                titleRef.current.innerHTML = `Round: <img style='${x_css}' src='${X_icon}'>`;
+                e.target.innerHTML = `<img style='${O_css}' src='${O_icon}'>`;
+                data[num]="o";
+                setCount(++count);
+            }
+            checkWin();
+        }
+    }
+
     //win checker
     const checkWin = () => {
         if(data[0]===data[1] && data[1]===data[2] && data[2]!==""){
@@ -67,6 +80,8 @@ export default function TicTacToe(){
             let locdat = data.filter(Boolean).length;
             if(locdat===9){
                 titleRef.current.innerHTML = `Draw!`;
+                setDraws(++draw);
+                score();
             }
         }
     }
@@ -75,23 +90,28 @@ export default function TicTacToe(){
         setLock(true);
         if(winner==="x"){
             titleRef.current.innerHTML = `Congrats! <img style='${x_css}' src=${X_icon}>`;
+            setXWins(++xWins);
+
         }else if(winner==="o"){
             titleRef.current.innerHTML = `Congrats! <img style='${o_css}'src=${O_icon}>`;
+            setOWins(++oWins);
         }
+        score();
     }
 
     const reset = () => {
         setLock(false);
-        data = ["","","","","","","","",""];
+        data = ['','','','','','','','',''];
         titleRef.current.innerHTML = '<span>React</span> Tic Tac Toe';
         box_array.map((e)=>{
-            e.current.innerHTML = ""
+            e.current.innerHTML = ''
         })
     }
 
     return (
         <div className="container">
             <h1 className="title" ref={titleRef}><span>React</span> Tic Tac Toe</h1>
+            <h1 className="title" ref={roundRef}></h1>
             <div className="board">
                 <div className="row1">
                     <div className="boxes" ref={box1} onClick={(e)=>{toggle(e,0)}}></div>
